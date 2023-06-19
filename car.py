7ba35c4e030738bdcce4ab09522631a0cb62d47f
sakp7 <119031981+sakp7@users.noreply.github.com>
2023-06-19 20:35:32 +0530

    Add files via upload

--- car.py
+++ car.py
@@ -1,19 +1,14 @@
-# Importing the neccesary packages
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-import streamlit as st
-
-# Loading the dataset
 df=pd.read_csv("C:/Users/saket/OneDrive/Desktop/dataset.csv")
 
 
-# Data preprocessing 
+
+from sklearn.preprocessing import LabelEncoder
 le=LabelEncoder()
-#label Encoding the data
+
 x=le.fit_transform(df["company"])
 df.drop("company",axis=1,inplace=True)
 df.insert(1,"company",x)
@@ -22,23 +17,26 @@ print(df)
 
 X=df[['company','year','kms_driven','fuel_type']]
 y=df['Price']
-# spliting the data
+
+from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
 
-# Training the data
+
 model=LinearRegression()
 model.fit(X_train,y_train)
 
 
-# Predicting the output
+
 y_pred=model.predict(X_test)
 
 print(r2_score(y_test,y_pred))
 
 
+import streamlit as st
 
-st.title("CAR PRICE PREDICTION")
+st.title("Car Price Prediction")
 km=st.number_input("Enter kilometers driven")
+
 year=st.number_input("Enter year")
 
 
@@ -54,3 +52,80 @@ li=[9,year,km,ft]
 predict=model.predict([li])
 if st.button("Submit"):
     st.success(predict)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
